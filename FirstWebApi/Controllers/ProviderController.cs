4fef5441b46f6df96c3f1e99f2a6f610af6ece76
Vikas Upadhyay <vikas.92543@gmail.com>
2022-04-15 14:24:53 +0530

    test method on fiddler

--- FirstWebApi/Controllers/ProviderController.cs
+++ FirstWebApi/Controllers/ProviderController.cs
@@ -6,29 +6,85 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using FirstWebApi.Models;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
 
 namespace FirstWebApi.Controllers
 {
+    //public class ProviderController : ApiController
+    //{
+    //public List<Provider> GetProviders()
+    //{
+    //    List<Provider> p = new List<Provider>();
+
+    //    var dataTables = Provider.GetProviderData();
+
+    //    foreach (DataRow data in dataTables.Tables[0].Rows)
+    //    {
+
+    //        p.Add(new Provider()
+    //        {
+    //            ProviderId = Convert.ToInt32(data[0].ToString()),
+    //            ProviderName = data[1].ToString(),
+    //            ProviderType = data[2].ToString()
+    //        });
+    //    }
+
+    //    return p;
+    //}
+
+
+
     public class ProviderController : ApiController
     {
         public List<Provider> GetProviders()
         {
-            List<Provider> p = new List<Provider>();
-
-            var dataTables = Provider.GetProviderData();
-
-            foreach (DataRow data in dataTables.Tables[0].Rows)
-            {
-
-                p.Add(new Provider()
-                {
-                    ProviderId = Convert.ToInt32(data[0].ToString()),
-                    ProviderName = data[1].ToString(),
-                    ProviderType = data[2].ToString()
-                });
-            }
-
-            return p;
+            return Provider.GetProviderData();
         }
+
+
+
+        public Provider GetProviderById(int id)
+        {
+            return Provider.GetProviderDataByProviderId(id);
+        }
+
+        //[HttpPost]
+        //public void AddProvider(Object p)
+        //{
+        //    var provider = Newtonsoft.Json.JsonConvert.DeserializeObject<Provider>(p.ToString()); 
+
+        //    Provider.InsertProvider(provider);
+        //}
+
+
+        [HttpPost]
+        public void AddProvider([FromUri] Provider p)
+        {
+            //var provider = Newtonsoft.Json.JsonConvert.DeserializeObject<Provider>(p.ToString());
+
+            Provider.InsertProvider(p);
+        }
+
+        //[HttpPost]
+        //public void AddProvider([FromBody] Provider p)
+        //{
+        //    //var provider = Newtonsoft.Json.JsonConvert.DeserializeObject<Provider>(p.ToString());
+
+        //    Provider.InsertProvider(p);
+        //}
+
+        [HttpPatch]
+        public void UpdateProvider([FromBody] Provider P)
+        {
+            Provider.UpdateProvider(P);
+        }
+
+        [HttpDelete]
+        public void UpdateProvider(int id)
+        {
+            Provider.DeleteProvider(id);
+        }
+
     }
 }
--- FirstWebApi/Models/Provider.cs
+++ FirstWebApi/Models/Provider.cs
@@ -16,28 +16,57 @@ namespace FirstWebApi.Models
 
         private static string connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
 
-        internal static object GetProvider()
+        public static List<Provider> GetProviderData()
         {
-            throw new NotImplementedException();
-        }
-
-        public static DataSet GetProviderData()
-        {
-
+            List<Provider> p = new List<Provider>();
             SqlConnection conn = new SqlConnection(connectionString);
-
 
             conn.Open();
             SqlCommand command = new SqlCommand("select * from Provider", conn);
             SqlDataAdapter adaptor = new SqlDataAdapter(command);
             DataSet ds = new DataSet();
+
+
             adaptor.Fill(ds);
-            return ds;
+            foreach (DataRow data in ds.Tables[0].Rows)
+            {
+                p.Add(new Provider()
+                {
+                    ProviderId = Convert.ToInt32(data[0].ToString()),
+                    ProviderName = data[1].ToString(),
+                    ProviderType = data[2].ToString()
+                });
+            }
+
+            return p;
         }
 
-        internal void InsertProvider(Provider p)
+        public static Provider GetProviderDataByProviderId(int id)
         {
+            List<Provider> p = new List<Provider>();
+            SqlConnection conn = new SqlConnection(connectionString);
 
+            conn.Open();
+            SqlCommand command = new SqlCommand($"select * from Provider where providerid = {id}", conn);
+            SqlDataAdapter adaptor = new SqlDataAdapter(command);
+            DataSet ds = new DataSet();
+            adaptor.Fill(ds);
+
+            foreach (DataRow data in ds.Tables[0].Rows)
+            {
+                p.Add(new Provider()
+                {
+                    ProviderId = Convert.ToInt32(data[0].ToString()),
+                    ProviderName = data[1].ToString(),
+                    ProviderType = data[2].ToString()
+                });
+            }
+
+            return p.FirstOrDefault();
+        }
+
+        internal static void InsertProvider(Provider p)
+        {
             SqlConnection conn = new SqlConnection(connectionString);
 
             string insertCmd = $"insert into Provider (ProviderName, ProviderType) values ('{p.ProviderName}','{p.ProviderType}')";
@@ -46,9 +75,8 @@ namespace FirstWebApi.Models
             command.ExecuteNonQuery();
         }
 
-        internal void UpdateProvider(Provider p)
+        internal static void UpdateProvider(Provider p)
         {
-
             SqlConnection conn = new SqlConnection(connectionString);
 
             string insertCmd = $"update provider set providertype='{p.ProviderType}', providername='{p.ProviderName}' where providerid={p.ProviderId}";
@@ -66,18 +94,5 @@ namespace FirstWebApi.Models
             SqlCommand command = new SqlCommand(insertCmd, conn);
             command.ExecuteNonQuery();
         }
-
-        public static DataSet GetProviderDataByProviderId(int id)
-        {
-
-            SqlConnection conn = new SqlConnection(connectionString);
-
-            conn.Open();
-            SqlCommand command = new SqlCommand($"select * from Provider where providerid = {id}", conn);
-            SqlDataAdapter adaptor = new SqlDataAdapter(command);
-            DataSet ds = new DataSet();
-            adaptor.Fill(ds);
-            return ds;
-        }              
     }
 }
