b8b0736e24d6a373049883a43710328c001584f0
Vikas Upadhyay <vikas.92543@gmail.com>
2022-04-18 23:01:15 +0530

    javascript

---
+++ FirstWebApi/Content/JS/default.js
@@ -0,0 +1,10 @@
+﻿let provider = { providerid: 1, providername: 'testProvider' };
+console.log(provider);
+
+let i = 5, j = 9;
+console.log(i++);
+console.log(i);
+
+console.log(++j);
+//let z = i / j;
+//console.log(z);
